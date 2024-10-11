93974f264557068b860fcd4ec25d7dca712d05bd
Dawei Feng <imdavidf@gmail.com>
2024-10-11 19:49:38 +0000

    Use OpenCV 4.10.0

--- benchmark/src/opencv_benchmark.cpp
+++ benchmark/src/opencv_benchmark.cpp
@@ -17,18 +17,18 @@ static inline unsigned long long rdtsc(void) {
   return ((unsigned long long)lo) | (((unsigned long long)hi) << 32);
 }
 
-struct CocoImageDir {
+struct CocoImageMeta {
   std::filesystem::path path;
   int width;
   int height;
 };
 
-void TestImages(const CocoImageDir &imageDir) {
+void TestImages(const CocoImageMeta &imageMeta) {
   unsigned long long st;
   unsigned long long et;
   unsigned long long sum = 0;
 
-  for (const auto &p : std::filesystem::directory_iterator(imageDir.path)) {
+  for (const auto &p : std::filesystem::directory_iterator(imageMeta.path)) {
 
     cv::Mat image = cv::imread(p.path(), cv::IMREAD_GRAYSCALE);
 
@@ -51,32 +51,36 @@ void TestImages(const CocoImageDir &imageDir) {
     sum += (et - st);
   }
 
-  std::cout << "RDTSC Cycles Taken for Canny: " << sum * MAX_FREQ / BASE_FREQ
-            << "\n";
+  // TODO: Find out the correct number of instructions
+  double totalInstructions = imageMeta.width * imageMeta.height;
+  double cycles = sum * MAX_FREQ / BASE_FREQ;
+
+  std::cout << "RDTSC Cycles Taken for Canny: " << cycles << "\n";
+  std::cout << "Instructions Per Cycle" << totalInstructions / cycles << "\n";
 }
 
 int main(int argc, char *argv[]) {
   std::filesystem::path cocoImagePath = (std::string)argv[1];
   std::cout << "Coco image path: " << cocoImagePath << "\n";
 
-  CocoImageDir smallImageDir = {cocoImagePath / "500x500", 500, 500};
-  CocoImageDir mediumImageDir = {cocoImagePath / "800x800", 800, 800};
-  CocoImageDir largeImageDir = {cocoImagePath / "1024x1024", 1024, 1024};
+  CocoImageMeta smallImage = {cocoImagePath / "500x500", 500, 500};
+  CocoImageMeta mediumImage = {cocoImagePath / "800x800", 800, 800};
+  CocoImageMeta largeImage = {cocoImagePath / "1024x1024", 1024, 1024};
 
   try {
     std::cout << "================================================" << "\n";
-    std::cout << "Testing images in " << smallImageDir.path << "\n";
-    TestImages(smallImageDir);
+    std::cout << "Testing images in " << smallImage.path << "\n";
+    TestImages(smallImage);
     std::cout << "================================================" << "\n";
 
     std::cout << "================================================" << "\n";
-    std::cout << "Testing images in " << mediumImageDir.path << "\n";
-    TestImages(mediumImageDir);
+    std::cout << "Testing images in " << mediumImage.path << "\n";
+    TestImages(mediumImage);
     std::cout << "================================================" << "\n";
 
     std::cout << "================================================" << "\n";
-    std::cout << "Testing images in " << largeImageDir.path << "\n";
-    TestImages(largeImageDir);
+    std::cout << "Testing images in " << largeImage.path << "\n";
+    TestImages(largeImage);
     std::cout << "================================================" << "\n";
   } catch (const std::runtime_error &err) {
     std::cerr << "[ERROR] " << err.what() << "\n";
